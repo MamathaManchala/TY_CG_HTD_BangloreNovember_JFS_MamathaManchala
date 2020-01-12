a89678b683ac51ccc06de6c58377567f9664182c
MamathaManchala <mamathamanchala4@gmail.com>
2020-01-12 14:27:58 +0530

    files 4

--- BackEnd/spring/user/src/main/java/com/capgemini/user/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.user.dao;
-
-import com.capgemini.user.dto.User;
-
-public interface UserDAO {
-	public boolean register(User user);
-	public User login(String email,String password);
-	
-}
--- BackEnd/spring/user/src/main/java/com/capgemini/user/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.user.service;
-
-import com.capgemini.user.dto.User;
-
-public interface UserService {
-	public boolean register(User user);
-	public User login(String email,String password);
-}
